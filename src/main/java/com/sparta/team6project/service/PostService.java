9bc998c9b1a2491dc81f9062560e05d661146260
hyeji <laon1551@naver.com>
2022-02-11 18:45:58 +0900

    게시글 삭제(로그인연결X)

--- src/main/java/com/sparta/team6project/service/PostService.java
+++ src/main/java/com/sparta/team6project/service/PostService.java
@@ -27,7 +27,7 @@ public class PostService {
                 .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 게시글입니다."));
 
         // 게시글 작성자 일치 확인
-        if (userDetails.getUsername() != foundPost.getPostUser()) {
+        if (!userDetails.getUsername().equals(foundPost.getPostUser())) {
             throw new IllegalArgumentException("타인이 작성한 게시글은 수정할 수 없습니다.");
         }
 
@@ -35,5 +35,21 @@ public class PostService {
         foundPost.editPost(postRequestDto);
         return postRepository.save(foundPost);
 
+    }
+
+    public Long deletePost(Long postId, UserDetailsImpl userDetails) {
+
+        // 게시글 유무 확인
+        Post foundPost = postRepository.findById(postId)
+                .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+
+        // 게시글 작성자 일치 확인
+        if (!userDetails.getUsername().equals(foundPost.getPostUser())) {
+            throw new IllegalArgumentException("타인이 작성한 게시글은 삭제할 수 없습니다.");
+        }
+
+        // 게시글 삭제
+        postRepository.deleteById(postId);
+        return postId;
     }
 }
