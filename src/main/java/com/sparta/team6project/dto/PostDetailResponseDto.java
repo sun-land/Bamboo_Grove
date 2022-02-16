0746b21d4aca853fd0b1f2012cd8e3311f4ef3c9
hyeji <laon1551@naver.com>
2022-02-16 10:36:41 +0900

    상세게시글 시간 포맷변경, 토큰 유효기간 3일로 늘림

--- src/main/java/com/sparta/team6project/dto/PostDetailResponseDto.java
+++ src/main/java/com/sparta/team6project/dto/PostDetailResponseDto.java
@@ -4,26 +4,26 @@ import com.sparta.team6project.model.Comment;
 import com.sparta.team6project.model.Post;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Getter
-public class PostResponseDto {
+public class PostDetailResponseDto {
     private String loginUser;
     private Long postId;
     private String postUser;
     private String title;
     private String contents;
-    private LocalDateTime createdAt;
+    private String createdAt;
     private List<Comment> comments;
 
-    public PostResponseDto(String loginUser, Post post) {
+    public PostDetailResponseDto(String loginUser, Post post) {
         this.loginUser = loginUser;
         this.postId = post.getId();
         this.postUser = post.getPostUser();
         this.title = post.getTitle();
         this.contents = post.getContents();
-        this.createdAt = post.getCreatedAt();
+        this.createdAt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(post.getCreatedAt());
         this.comments = post.getComments();
     }
 
