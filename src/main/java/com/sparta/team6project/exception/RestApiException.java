7ac6bdc6110941b4b7294e55c31d41c9caecd4e2
DESKTOP-I2T6KB6\llsmk <llsmkr1057@gmail.com>
2022-02-17 10:57:27 +0900

    no message

--- src/main/java/com/sparta/team6project/exception/RestApiException.java
+++ src/main/java/com/sparta/team6project/exception/RestApiException.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Setter
 public class RestApiException {
+    // 동작 성공은 true, 실패는 false
     private boolean ok;
+    // 에러원인메시지
     private String errorMessage;
 }
